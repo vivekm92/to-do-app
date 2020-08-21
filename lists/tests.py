a4547385ce0e5b819fc7b7e99eeefe16d1605f25
vivekm <viv.mttl@gmail.com>
2020-08-22 02:20:33 +0530

    Refator home page view to use a template.

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ import logging
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 logger = logging.getLogger()
@@ -10,14 +11,12 @@ logger.setLevel('DEBUG')
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
 
-    def test_home_page_returns_correct_htmll(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html'))
+        html = response.content.decode('utf-8').strip()
+        self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
