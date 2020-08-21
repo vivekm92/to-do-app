05021888f1cee103fc4e01064371f80a4389b8a2
vivekm <viv.mttl@gmail.com>
2020-08-22 02:42:15 +0530

    Front Page html now generated from a template.

--- functional_tests.py
+++ functional_tests.py
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_items')
         self.assertEqual(inputbox.get_attribute('placeholder'),
             'Enter a to-do item')
 
@@ -28,8 +28,9 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table")
 
         self.fail('Finish the test!')
 
